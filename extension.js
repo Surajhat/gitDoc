67241416ec49d910beed2263f265c4ef7004fdeb
suraj-narayan <surajnarayan_2k19co397@dtu.ac.in>
2024-09-16 22:30:45 +0530

    new page added

--- extension.js
+++ extension.js
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+const SnippetInputPanel = require('./src/docSet');
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -17,14 +19,41 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('docgit.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showErrorMessage('Hello World from DocGit!');
+	const docSet = vscode.commands.registerCommand('docgit.docSet', (snippetText) => {
+		SnippetInputPanel.createOrShow(context.extensionUri, snippetText);
+	});``
+
+	const docSelectedCode = vscode.commands.registerCommand('docgit.docSelectedCode', function () {
+
+		const editor = vscode.window.activeTextEditor;
+		if(!editor) {
+			vscode.window.showErrorMessage('No active editor found');
+			return;
+		}
+		const selection = editor.selection;
+		vscode.window.showInformationMessage(JSON.stringify(selection));
+		const selectedText = editor.document.getText(selection);
+
+		const lines = selectedText.split('\n').splice(0, 2).join('\n');
+		console.log(lines);
+		vscode.commands.executeCommand('docView.focus');
+		vscode.commands.executeCommand('docgit.docSet', lines);
+
+		
+
+
+
+
+		
 	});
 
-	context.subscriptions.push(disposable);
+
+
+
+
+
+	context.subscriptions.push(docSelectedCode);
 }
 
 // This method is called when your extension is deactivated
